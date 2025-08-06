5e2115a85c1789efc1cb40e5383e661c87e0174c
Roopesh Tayaloor <roopesht@gmail.com>
2025-08-06 10:11:01 +0530

    Add /java/maps/1/concept_1.java

---
+++ java/maps/1/concept_1.java
@@ -0,0 +1,9 @@
+{
+  "cells": [],
+  "metadata": {
+    "title": "Java Map Basics with HashMap",
+    "description": "A beginner-friendly Java program demonstrating the creation and usage of HashMap to store key-value pairs."
+  },
+  "nbformat": 4,
+  "nbformat_minor": 5
+}
