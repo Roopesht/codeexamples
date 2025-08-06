9b354deb65339f8b648600eeabb89fdb2ba80e34
Roopesh Tayaloor <roopesht@gmail.com>
2025-08-06 10:14:53 +0530

    Add /java/maps/1/concept_2.java

---
+++ java/maps/1/concept_2.java
@@ -0,0 +1,9 @@
+{
+  "cells": [],
+  "metadata": {
+    "title": "Accessing and Modifying Map Data in Java",
+    "description": "Learn how to add, retrieve, update, and remove data in a Java Map using put(), get(), and remove() methods, with beginner-friendly explanations and examples."
+  },
+  "nbformat": 4,
+  "nbformat_minor": 2
+}
